90124d12095ebdc20887454e38f6b6cf51dc64cb
leecheonguk <dlcjsrnr0860@naver.com>
2024-11-13 03:06:00 +0900

    주소 10개

--- fetchVideos.js
+++ fetchVideos.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const API_KEY = process.env.YOUTUBE_API_KEY;  // GitHub Secrets에서 가져오기
 const CHANNEL_ID = 'UCfItTXw28rd2Z2DSw0UqAXg';  // 특정 유튜브 채널 ID
-const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=7&order=date&type=video&key=${API_KEY}`;
+const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=10&order=date&type=video&key=${API_KEY}`;
 
 async function fetchYouTubeVideos() {
   if (!API_KEY) {
